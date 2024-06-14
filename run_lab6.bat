449121a5b9efdc2b0442cb32e46254afbc623109
Bogdan Liutikov <lbi2001@yandex.ru>
2024-06-14 17:27:01 +0300

    fix lab6

--- run_lab6.bat
+++ run_lab6.bat
@@ -1,7 +1,5 @@
-kubectl apply -f k8s/mssql-pv.yaml
-kubectl apply -f k8s/mssql-pv-claim.yaml
-kubectl apply -f k8s/mssql-service.yaml
-kubectl apply -f k8s/mssql-deploy.yaml
+kubectl apply -f k8s/cassandra-service.yaml
+kubectl apply -f k8s/cassandra-statefulset.yaml
 kubectl apply -f k8s/spark-lab6.yaml
 
 kubectl describe lab6-driver --namespace spark-operator
